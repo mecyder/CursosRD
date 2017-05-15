ab1bcbd48c0456e3c9f1a715375b04b908b8e15a
w.urena <w.urena@TC01TICLT10>
2017-05-15 12:38:50 -0400

    dd

---
+++ CursosRD/Entidades/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Usuarios
+    {
+        public string Name { get; set; }
+        public string Pass { get; set; }
+        public string User { get; set; }
+        public DateTime Created { get; set; }
+        public Boolean? State { get; set; }
+
+    }
+}
