9b25ccf00bc6edd3f1206b1a138e652ab9d433a0
w.urena <w.urena@TC01TICLT10>
2017-05-19 08:36:59 -0400

    Interfaces

---
+++ CursosRD/ClassLibrary1/ICursos.cs
@@ -0,0 +1,26 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLogic
+{
+    interface ICursos
+    {
+        void Adiccionar(Curso Entidad);
+        IQueryable<Curso> AsQueryable();
+        IEnumerable<Curso> Buscar(Expression<Func<Curso, bool>> predicado);
+        void Eliminar(Curso Entidad);
+        void Guardar();
+        void Modificar(Curso Entidad);
+        IEnumerable<Curso> TraerTodo();
+        Curso traeruno(Expression<Func<Curso, bool>> predicado);
+        Curso TraerUnoPorID(int Id);
+
+
+
+    }
+}
